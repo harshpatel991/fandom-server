59d23e2bb67a304836ccb0aa6582c50802dc622f
dsmith74 <dbsmith93@gmail.com>
2015-04-26 23:59:20 +0000

    Added Show Schema

---
+++ models/show.js
@@ -0,0 +1,25 @@
+// Load required packages
+var mongoose = require('mongoose');
+
+// Define our beer schema
+var ShowSchema   = new mongoose.Schema({
+  id: Number,
+  cast: [String],
+  first_date: Date,
+  genres: [String],
+  imdb_id: String,
+  imdb_rating: Number,
+  img_filename: String,
+  keywords: [String],
+  last_date: Date,
+  name: String,
+  networks: [String],
+  num_eps: Number,
+  num_seasons: Number,
+  remote_img_url: String,
+  seasons: [Number],
+  summary: String
+});
+
+// Export the Mongoose model
+module.exports = mongoose.model('Llama', LlamaSchema);
