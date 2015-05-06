1d7e017ae7ff1d1b92f585ddf7b618787b4ebc10
Harsh <hpatel90@illinos.edu>
2015-05-05 22:18:52 -0500

    Fix for comments voting

--- server.js
+++ server.js
@@ -29,7 +29,8 @@ var allowCrossDomain = function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept", "Access-Control-Allow-Origin");
   res.header("Allow-Credentials", true);
-  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Credentials", true)
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT");
 
   next();
 };
